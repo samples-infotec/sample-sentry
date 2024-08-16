a38f862410e7296f52dff4b159581bc356e76b57
Erik Valdivieso Diaz <crip.damcad125@conahcyt.mx>
2024-08-16 03:15:34 -0600

    build: :wrench: agrega componentes extras de vue boostrap

--- src/main/webapp/app/shared/config/config-bootstrap-vue.ts
+++ src/main/webapp/app/shared/config/config-bootstrap-vue.ts
@@ -21,6 +21,9 @@ import {
   BAlert,
   BModal,
   VBModal,
+  BContainer,
+  BCol,
+  BRow,
   BFormDatepicker,
   BInputGroup,
   BInputGroupPrepend,
@@ -50,6 +53,9 @@ export function initBootstrapVue(vue) {
   vue.component('b-nav-item-dropdown', BNavItemDropdown);
   vue.component('b-modal', BModal);
   vue.directive('b-modal', VBModal);
+  vue.component('b-container', BContainer);
+  vue.component('b-col', BCol);
+  vue.component('b-row', BRow);
   vue.component('b-form-datepicker', BFormDatepicker);
   vue.component('b-input-group', BInputGroup);
   vue.component('b-input-group-prepend', BInputGroupPrepend);
