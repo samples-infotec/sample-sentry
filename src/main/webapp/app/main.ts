a219cc60dce516bd958e4db8db0f7c208ecd06a3
Erik Valdivieso Diaz <crip.damcad125@conahcyt.mx>
2024-08-16 00:09:32 -0600

    feat: :sparkles: agrega configuración para replay

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -101,11 +101,16 @@ axios
           }
           return event;
         },
+        replaysSessionSampleRate: tracesSampleRate,
+        // If the entire session is not sampled, use the below sample rate to sample
+        // sessions when an error occurs.
+        replaysOnErrorSampleRate: 1.0,
         integrations: [
           new Integrations.BrowserTracing({
             routingInstrumentation: Sentry.vueRouterInstrumentation(router),
             tracingOrigins: ['localhost', 'demo-sentry-7f000101.nip.io', /^\//],
           }),
+          new Sentry.Replay(),
         ],
         // This sets the sample rate to be 10%. You may want this to be 100% while
         // in development and sample at a lower rate in production
